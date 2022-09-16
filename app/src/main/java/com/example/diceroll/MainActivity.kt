e51ad15d7ea89be0f46ad026dc41babc580f073a
subash kc <subaazkc@gmail.com>
2022-09-16 22:24:12 +0545

    first commit

---
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.diceroll
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+fun main() {
+    val myFirstDice = Dice()
+    println(myFirstDice.sides)
+    myFirstDice.roll()
+}
+
+class Dice {
+    var sides = 6
+
+    fun roll() {
+        val randomNumber = (1..6).random()
+        println(randomNumber)
+    }
+}
